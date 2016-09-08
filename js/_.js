499750343f6c16cc942e9db65eb83e9a31dbc0d0
msnrhnd <msnrhnd@gmail.com>
2016-09-09 00:51:10 +0900

    test

--- js/_.js
+++ js/_.js
@@ -34,10 +34,10 @@ $(document).ready(function () {
       }
       facematerial = new THREE.MeshFaceMaterial(materials);
     }
-    mesh = new THREE.SkinnedMesh(geometry, facematerial);
+    mesh = new THREE.SkinnedMesh(geometry, shadermaterial);
     mesh.scale.set(1, 1, 1);
     mesh.position.set(0, 0, 0);
-    meshEdge = new THREE.SkinnedMesh(geometry, shadermaterial);
+    meshEdge = mesh.clone();
     scene.add(mesh);
     sceneEdge.add(meshEdge);
   }
@@ -50,37 +50,45 @@ $(document).ready(function () {
     sceneEdge = new THREE.Scene();
     ambientLight = new THREE.AmbientLight('white');
     scene.add(ambientLight);
-    sceneEdge.add(ambientLight);
     globalLight = new THREE.DirectionalLight('white');
+    scene.add(globalLight);
+    sceneEdge.add(ambientLight);
     globalLight.position.set(1, 0, 0).normalize();
     var loader = new THREE.JSONLoader(true);
     loader.load('mesh/sheep.js', createScene);
     renderer = new THREE.WebGLRenderer({
       antialias: true
     });
-    renderer.sortObjects = false;
-    scene.add(globalLight);
     renderer.setSize(WIDTH, HEIGHT);
+    renderer.setClearColor('white', 1);
     renderer.setClearColor('lightgray', 1);
+    renderer.sortObjects = false;
+    renderer.autoClear = false;
     var canvas = document.getElementById('canvas');
     canvas.appendChild(renderer.domElement);
   }
 
   function render() {
-    if (mesh) {
-      mesh.material.side = THREE.FrontSide;
-//      mesh.material.uniforms.edgeColor.value = new THREE.Vector4(0, 0, 0, 0);
-//      mesh.material.uniforms.edge.value = false;
-    }
-    renderer.render(scene, camera);
-    if (mesh) {
-      mesh.material.side = THREE.BackSide;
+    renderer.clear();
+    if (meshEdge) {
+      meshEdge.material.side = THREE.BackSide;
       meshEdge.material.uniforms.edgeColor.value = new THREE.Vector4(0, 0, 0, 1);
       meshEdge.material.uniforms.edge.value = true;
     }
-//    renderer.render(sceneEdge, camera);
+    renderer.render(sceneEdge, camera);
+    if (mesh) {
+      meshEdge.material.side = THREE.FrontSide;
+      mesh.material.uniforms.edgeColor.value = new THREE.Vector4(0, 0, 0, 0);
+      mesh.material.uniforms.edge.value = false;
+    }
+    renderer.render(scene, camera);
     requestAnimationFrame(render);
   }
   init();
   render();
 });
+
+
+
+
+
